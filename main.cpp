4a0f862a29d11950fa5b07c7abc487700aca57dd
ano0406 <m20000406k@gmail.com>
2020-05-02 14:42:56 +0900

    VSCodeに対応

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <limits.h>
+#include <queue>
+#include <map>
+#include <set>
+#include <iomanip>
+#include <bitset>
+#include <cassert>
+#include <random>
+#include <functional>
+#include <stack>
+#include <iomanip>
+#include <cassert>
+//#include <boost/multiprecision/cpp_int.hpp>
+#include <complex>
+#include <cstdio>
+#include <list>
+#include <bitset>
+
+//< in.txt > out.txt
+using namespace std;
+//std::ios::sync_with_stdio(false);
+//std::cin.tie(0);
+const long long MOD = 1e9 + 7;
+const long long INF = 1e18;
+typedef long long LL;
+typedef long double LD;
+typedef pair<LL, LL> PLL;
+typedef pair<LD, LL> pdl;
+typedef pair<LD, LD> pdd;
+typedef vector<LL> VLL;
+typedef vector<VLL> VVLL;
+typedef unsigned long long ULL;
+//typedef boost::multiprecision::cpp_int bigint;
+
+int main() {
+	//std::ios::sync_with_stdio(false);
+	//std::cin.tie(0);
+    int a;
+    cin >> a;
+    cout << a + 2 << "\n";
+    cin >> a;
+	return 0;
+}
