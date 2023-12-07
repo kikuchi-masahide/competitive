c1eccceaac3538c2b7ca1f2aad161a8d58f014fb
菊池雅英 <m20000406k@gmail.com>
2023-12-07 10:49:21 +0900

    feat: rust lib
    - upper/lower bound
    - graph doubling
    - scc

--- rust/src/mylib/bisearch_array.rs
+++ rust/src/mylib/bisearch_array.rs
@@ -1,12 +1,12 @@
-use cargo_snippets::snippet;
+use cargo_snippet::snippet;
 
 #[snippet("bisearch_array")]
-// arr[e-1] <= x < arr[e]
-// なるeを返す(0 <= e <= n)
-fn array_bisearch<T>(arr: &Vec<T>, x: T) -> usize
+fn lower_bound<T>(arr: &Vec<T>, x: T) -> usize
 where
     T: std::cmp::PartialOrd,
 {
+    // arr[e-1] <= x < arr[e]
+    // なるeを返す(0 <= e <= n)
     if x < arr[0] {
         return 0;
     }
@@ -15,6 +15,29 @@ where
     while e - s > 1 {
         let m = (s + e) / 2;
         if arr[m] <= x {
+            s = m;
+        } else {
+            e = m;
+        }
+    }
+    return e;
+}
+
+#[snippet("bisearch_array")]
+fn upper_bound<T>(arr: &Vec<T>, x: T) -> usize
+where
+    T: std::cmp::PartialOrd,
+{
+    // arr[e-1] < x <= arr[e]
+    // なるeを返す(0 <= e <= n)
+    if x <= arr[0] {
+        return 0;
+    }
+    let mut s = 0;
+    let mut e = arr.len();
+    while e - s > 1 {
+        let m = (s + e) / 2;
+        if arr[m] < x {
             s = m;
         } else {
             e = m;
